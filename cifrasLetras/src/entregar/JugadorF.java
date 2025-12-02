257c3f34566198a847c986657f712ee9d0f922f3
sofisofs <sofiablanco.c@icloud.com>
2025-12-02 22:35:27 +0100

    ut1y2 cifras y letras entregado

--- cifrasLetras/src/entregar/JugadorF.java
+++ cifrasLetras/src/entregar/JugadorF.java
@@ -1,26 +1,80 @@
 package entregar;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.Random;
+import java.util.concurrent.Semaphore;
+
+/*
+ * @author Ashley Leon y Sofia Blanco
+ * 
+ * */
 
 public class JugadorF extends Thread {
+
+	/* atributo enum para saber si es cifra o letra */
+	private TipoHilo cifraOletra;
+
+	/* constructor para poder usarlo en el main */
+	public JugadorF(TipoHilo cifraOletra) {
+		this.cifraOletra = cifraOletra;
+	}
+
 	Random r = new Random();
-	
-	public static FileOutputStream fosSalida ;
+	public final static Semaphore SEM1 = new Semaphore(1); // verde, una clase un semaforo
 
 	@Override
 	public void run() {
-		comprobarCrearTXT();
+		int contador = 0;
+
 		try {
-			fosSalida = new FileOutputStream("salida.txt", true);
-			Thread.sleep((long) (Math.random() * 1000));
-			
+			/* mientras no llegue el contador a 20 */
+			while (contador < 20) {
+				System.out.println("[B-llega] se mete al bucle");
+
+				/* que vayan despacio */
+				Thread.sleep(1000);
+				System.out.println("[B-llega] se va a dormir");
+
+				/* si es hilo cifras escribes si es letras escribes */
+				if (cifraOletra == TipoHilo.CIFRAS) {
+					/* escribes cifras y letras */
+					/* antes tienes que dar luz verde */
+					SEM1.acquire();
+					MainF.fosSalida.write(cifrasRandom(r));
+					//System.out.println("[B-llega] escribe cifras "+cifrasRandom(r));
+					SEM1.release();
+				} else {
+					SEM1.acquire();
+					MainF.fosSalida.write(letraRandom(r));
+					//System.out.println("[B-llega] escribe letras "+letraRandom(r));
+					SEM1.release();
+				}
+
+				/* has escrito en el fichero asi que suma uno y sigues */
+				contador++;
+
+				/* si llegas a 20 */
+				if (contador == 20) {
+					System.out.println("[llega] se mete al if de ganador");
+					/* le dices quien ganas!! */
+					MainF.ganador = cifraOletra.toString(); // a la variable ganador que es compartida le metes tu variable tipo de hilo (saca la que antes se meta al if)
+
+					/* te metes en lo compartido */
+					synchronized (MainF.sincronizador) {
+						System.out.println("[llega] notificar al main del ganador");
+						/* avisas al ganador */
+						MainF.sincronizador.notify(); 
+					}
+				}
+
+			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public enum TipoHilo {
+		CIFRAS, LETRAS
 	}
 
 	public static char letraRandom(Random r) {
@@ -35,20 +89,6 @@ public class JugadorF extends Thread {
 		char[] arrayCifras = cifras.toCharArray();
 
 		return arrayCifras[r.nextInt(arrayCifras.length)];
-	}
-
-	public static void comprobarCrearTXT() {
-		File ficherotxt = new File("salida.txt");
-		if (!ficherotxt.exists()) {
-			System.err.println("ERROR. El fichero que estas intentando leer no existe(salida.txt)");
-			try {
-				System.out.println("INFO. Se procede a la creacion de salida.txt)");
-				ficherotxt.createNewFile();
-			} catch (IOException io) {
-				io.printStackTrace();
-			}
-		}
-
 	}
 
 }
--- cifrasLetras/src/entregar/MainF.java
+++ cifrasLetras/src/entregar/MainF.java
@@ -1,26 +1,67 @@
 package entregar;
 
-public class MainF {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
-	public static final Object sincronizador = new Object();
+import entregar.JugadorF.TipoHilo;
+/*
+ * @author Ashley Leon y Sofia Blanco
+ * 
+ * */
+
+public class MainF {
+	/* objeto sincronifazor que va a ser un recurso compartido */
+	public static Object sincronizador = new Object();
 	public static String ganador = "";
+
+	/* abres fichero de lectura y compartido */
+	public static FileOutputStream fosSalida;
 
 	public static void main(String[] args) {
 		try {
-			JugadorF cifras = new JugadorF();
-			JugadorF letras = new JugadorF();
-			
-			synchronized (sincronizador) {
-				cifras.start();
-				letras.start();
-				sincronizador.wait();
-			}
+			/* Tenemos el recurso compartido? */
+			comprobarCrearTXT();
 
+			/* abres tuberia para escribir */
+			fosSalida = new FileOutputStream("salida.txt");
+
+			JugadorF cifras = new JugadorF(TipoHilo.CIFRAS);
+			JugadorF letras = new JugadorF(TipoHilo.LETRAS);
+
+			/* empezamos dos hilos */
+			cifras.start();
+			letras.start();
+
+			/* te metes al recurso compartido */
+			synchronized (sincronizador) {
+				/* esperas al ganador */
+				System.out.println("[llega] esperar al ganador");
+				sincronizador.wait(); // esperas al ganador
+			}
+			/* cierras */
+			//fosSalida.close();
+
+			/* imprimes quien gaan */
 			System.out.println("El ganador es: " + ganador);
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static void comprobarCrearTXT() {
+		File ficherotxt = new File("salida.txt");
+		if (!ficherotxt.exists()) {
+			System.err.println("ERROR. El fichero que estas intentando leer no existe(salida.txt)");
+			try {
+				System.out.println("INFO. Se procede a la creacion de salida.txt)");
+				ficherotxt.createNewFile();
+			} catch (IOException io) {
+				io.printStackTrace();
+			}
+		}
+
 	}
 
 }
