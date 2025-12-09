df3a2d97a47dca58b18c928acb099a93c3f1f6b6
sofisofs <sofiablanco.c@icloud.com>
2025-12-09 04:08:26 +0100

    lo de antes. sigue con alta baja y pasas a listar noticias

---
+++ NoticiasPracticaFinal_03ddi/src/uiPaneles/PanelUsuarioNoticias.java
@@ -0,0 +1,10 @@
+package uiPaneles;
+
+import javax.swing.JPanel;
+
+public class PanelUsuarioNoticias extends JPanel{
+	public PanelUsuarioNoticias() {
+		setLayout(null);
+		setBounds(0, 0, 1200, 800);
+	}
+}
---
+++ NoticiasPracticaFinal_03ddi/src/uiPaneles/PanelUsuarioNuevo.java
@@ -0,0 +1,13 @@
+package uiPaneles;
+
+import javax.swing.JPanel;
+
+public class PanelUsuarioNuevo extends JPanel {
+	public PanelUsuarioNuevo() {
+		setLayout(null);
+		setBounds(0, 0, 1200, 800);
+		
+		
+		
+	}
+}
